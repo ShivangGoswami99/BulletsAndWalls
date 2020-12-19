acd9b455b319162f58d16931e8ff433239898e71
ShivangGoswami99 <74864447+ShivangGoswami99@users.noreply.github.com>
2020-12-19 19:10:17 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,6 +8,11 @@ function setup() {
   weight=random(30,52);
   thickness=random(22,83);
 
+  bullet=createSprite(50,200,50,5);
+bullet.velocityX=speed;
+bullet.shapeColor=color(255);
+
+
   wall=createSprite(1200,200,thickness,height/2);
   wall.shapeColor=color(80,80,80);
 }
@@ -25,7 +30,7 @@ function draw() {
     }
 
    
-    if(damage<100){
+    if(damage<10){
       wall.shapeColor=color(0,255,0);
     }
 
